afe8e9787263acbb5158abe7b1e36adbd502cf60
srina7h <srinathmadhappan1@gmail.com>
2022-08-23 10:29:09 +0530

    "22/08/2022" Loosely Coupled

--- src/main/java/com/nseit/String/Application.java
+++ src/main/java/com/nseit/String/Application.java
@@ -3,7 +3,7 @@ package com.nseit.String;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/nseit/String/BinarySearchImpl.java
+++ src/main/java/com/nseit/String/BinarySearchImpl.java
@@ -2,7 +2,7 @@ package com.nseit.String;
 
 public class BinarySearchImpl {
     public int binarySearch(int[] numbers, int numberToSearch) {
-        //BubbleSortAlg bubblesort = new BubbleSortAlg();
+         BubbleSortAlg bubblesort = new BubbleSortAlg();
         QuickSort quickSort = new QuickSort();
         int[] sortedNumbers = quickSort.sort(numbers);
         return 12;
